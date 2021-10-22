f0dfee96c087227dd119e7d81271a9a17eb0b9b9
worspy <79117124+Jln1224@users.noreply.github.com>
2021-10-22 20:15:41 -0300

    Add files via upload

---
+++ Calculadora dinâmica/script.js
@@ -0,0 +1,54 @@
+    
+//SCRIPT BY L0XYS
+
+    //Aplicação calculadora dinâmica.
+        
+        //Queremos um número inicial
+        //Queremos um sinal (tipo de operação)
+        //Quero um número final
+
+        /*var calculadora = [];
+                           
+        document.querySelector('input[type=submit]')
+        .addEventListener('click', ()=>{
+            
+            var numeroInicial = document.querySelector('[prompt]')
+            var sinal = document.querySelector('[prompt]')
+            var numeroFinal = document.querySelector('[prompt]')
+        });
+         */
+        function FuncaoDaCalc(){
+        var numeroInicial = prompt ('informe o primeiro número...');
+
+        var sinal = prompt ('informe um sinal da sua operação...');
+
+        while(sinal != '-' && sinal != '+' && sinal != '/' && sinal != '*'){
+            alert('Por favor informe um sinal válido');
+
+            sinal = prompt('informe um sinal da sua operação...');
+        }
+       
+
+
+        var numeroFinal = prompt('informe o número final...');
+
+        //Funções para converter para número uma string.
+
+        numeroInicial = parseInt(numeroInicial);
+        numeroFinal = parseInt(numeroFinal);
+
+        var resultadoFinal = 0;
+
+        if(sinal == '+'){
+            resultadoFinal = numeroInicial + numeroFinal;
+        }else if(sinal == '-'){
+            resultadoFinal = numeroInicial - numeroFinal;
+        }else if(sinal == '/'){
+            resultadoFinal = numeroInicial / numeroFinal;
+        }else if(sinal == '*'){
+            resultadoFinal = numeroInicial * numeroFinal;
+        }
+
+        alert("O resultado final é : " + resultadoFinal)
+    }
+
